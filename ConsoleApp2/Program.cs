65d3ce894dc92b1d26baa4acfb203283023f6f71
cathoDr-immalle <catho.driesen@immalle.be>
2017-12-17 11:09:55 +0100

    total seconds to hours minuts and seconds with a methode

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,30 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            tijdBereken(2549);
+            tijdBereken(3600);
+            tijdBereken(5489);
+            tijdBereken(131614154);
+
+            Console.Read();
         }
+
+        static void tijdBereken(int TotaalAantalSeconde)
+        {
+            int Uren = TotaalAantalSeconde / 3600;
+            int Minuten =(TotaalAantalSeconde - (Uren*3600)) / 60;
+            int seconden;
+
+            if (Minuten == 0)
+            {
+                seconden = 0;
+            }else
+            {
+                seconden =(TotaalAantalSeconde - (Uren * 3600) - (Minuten * 60));
+            } 
+            
+            Console.WriteLine("{0} seconden = {1} uren, {2} minuten en {3} seconden.", TotaalAantalSeconde, Uren, Minuten, seconden);
+        }
+
     }
 }
